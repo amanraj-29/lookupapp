4ad46b404d3db4caff994cb03499cda1c7d60ae2
rajesh-prodapt <rajeshkumar.k@prodapt.com>
2022-08-01 18:48:12 +0530

    Recommended plan tab

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard.component';
 import { BillingDetailsComponent } from './billing-details/billing-details.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { RecommendedPlansComponent } from './recommended-plans/recommended-plans.component';
 
 
 
@@ -18,6 +19,9 @@ const routes:Routes=[
     },
     {
       path:'deviceRates',loadChildren:()=>import('./device-rates/device-rates.module').then(val=>val.DeviceRatesModule)
+    },
+    {
+      path:'recPlans',component:RecommendedPlansComponent
     }
   ]
 },
@@ -26,7 +30,8 @@ const routes:Routes=[
 @NgModule({
   declarations: [
     DashboardComponent,
-    BillingDetailsComponent
+    BillingDetailsComponent,
+    RecommendedPlansComponent
   
   ],
   imports: [
---
+++ src/app/dashboard/recommended-plans/recommended-plans.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-plans',
+  templateUrl: './recommended-plans.component.html',
+  styleUrls: ['./recommended-plans.component.scss']
+})
+export class RecommendedPlansComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
