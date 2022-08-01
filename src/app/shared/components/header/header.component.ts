8de9d8dd3698e562af4283c0f92a41ad11cb7c81
rajesh-prodapt <rajeshkumar.k@prodapt.com>
2022-08-01 19:09:34 +0530

    Logout button display logic

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { UtilitiesService } from '../../services/utilities.service';
 @Component({
   selector: 'app-header',
@@ -7,7 +9,20 @@ import { UtilitiesService } from '../../services/utilities.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private utils:UtilitiesService) { }
+  showLogin=true;
+  constructor(private utils:UtilitiesService,private router:Router) { 
+    this.router.events.pipe(filter((val)=>val instanceof NavigationEnd)).subscribe((val:any)=>{ 
+      // console.log('nav url is:',val);
+    let urlArr=val.url.split('/');
+    // console.log('url Arr:',urlArr);
+      if(urlArr[urlArr.length-1]==='login'){
+this.showLogin=false;
+      }else{
+this.showLogin=true;
+
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
