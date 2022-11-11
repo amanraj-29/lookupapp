2e6a641332b5461b9110ab7ebaa9a8da54cb0f4d
amanraj-29 <amanraj.a@prodapt.com>
2022-11-11 13:44:05 +0530

    New Chanes Adjust yellow

--- src/app/dashboard/device-rates/device-rate-details/device-rate-details.component.ts
+++ src/app/dashboard/device-rates/device-rate-details/device-rate-details.component.ts
@@ -1,34 +1,95 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApisService } from 'src/app/shared/services/apis.service';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-device-rate-details',
   templateUrl: './device-rate-details.component.html',
-  styleUrls: ['./device-rate-details.component.scss']
+  styleUrls: ['./device-rate-details.component.scss'],
 })
 export class DeviceRateDetailsComponent implements OnInit {
+  deviceDetails: any = {};
+  nonMigratableBy: Array<string> = [];
 
-  deviceDetails:any={};
-  nonMigratableBy:Array<string>=[];
-  constructor(private router:Router,private location:Location,private actRoute:ActivatedRoute,private apiService:ApisService) {
-//     let id=this.actRoute.snapshot.paramMap.get('id');
-//     console.log('id is:',id);
-//     this.apiService.deviceDetails(id).subscribe(val=>{
-// this.deviceDetails=val.result[0];
-//     })
-this.actRoute.data.subscribe(deviceDet=>{
-// console.log('details are:',deviceDet);
-this.deviceDetails=deviceDet['deviceDetails']['result'][0]
-})
-    
-   }
-   goBack(){
-     this.location.back();
-   }
-  ngOnInit(): void {
-    
+  DevicebuttonClass: any;
+  DevicebuttonMessage: any;
+
+  SimGenbuttonClass: any;
+  SimGenbuttonMessage: any;
+
+  RatePlanbuttonClass: any;
+  RatePlanbuttonMessage: any;
+
+  constructor(
+    private router: Router,
+    private location: Location,
+    private actRoute: ActivatedRoute,
+    private apiService: ApisService
+  ) {
+    //     let id=this.actRoute.snapshot.paramMap.get('id');
+    //     console.log('id is:',id);
+    //     this.apiService.deviceDetails(id).subscribe(val=>{
+    // this.deviceDetails=val.result[0];
+    //     })
+    this.actRoute.data.subscribe((deviceDet) => {
+      console.log('details are:', deviceDet);
+      this.deviceDetails = deviceDet['deviceDetails']['result'][0];
+
+      if (this.deviceDetails?.Migratable_By_Device_status_code == 0) {
+        this.DevicebuttonClass = 'nmg';
+        this.DevicebuttonMessage = 'Action Required';
+      }
+      if (this.deviceDetails?.Migratable_By_Device_status_code == 1) {
+        this.DevicebuttonClass = 'mg';
+        this.DevicebuttonMessage = 'Ready';
+      }
+      if (this.deviceDetails?.Migratable_By_Device_status_code == 2) {
+        this.DevicebuttonClass = 'mgar';
+        this.DevicebuttonMessage = 'Possible Action Required';
+      }
+      if (this.deviceDetails?.Migratable_By_Device_status_code == 3) {
+        this.DevicebuttonClass = 'nmg';
+        this.DevicebuttonMessage = 'Action Required';
+      }
+      if (this.deviceDetails?.Migratable_By_Device_status_code == 'P_ACT_REQ') {
+        this.DevicebuttonClass = 'mgar';
+        this.DevicebuttonMessage = 'Possible Action Required';
+      }
+
+
+      if (this.deviceDetails?.SIM_Gen_Status == "Migratable") {
+        this.SimGenbuttonClass = 'mg';
+        this.SimGenbuttonMessage = 'Ready';
+      }
+      if (this.deviceDetails?.SIM_Gen_Status == "Possible_Action_Required") {
+        this.SimGenbuttonClass = 'mgar';
+        this.SimGenbuttonMessage = 'Possible Action Required';
+      }
+      if (this.deviceDetails?.SIM_Gen_Status == "Non-Migratable") {
+        this.SimGenbuttonClass = 'nmg';
+        this.SimGenbuttonMessage = 'Action Required';
+      }
+
+      if (this.deviceDetails?.Migratable_by_rate_plan == "Migratable") {
+        this.RatePlanbuttonClass = 'mg';
+        this.RatePlanbuttonMessage = 'Ready';
+      }
+      if (this.deviceDetails?.Migratable_by_rate_plan == "Possible_Action_Required") {
+        this.RatePlanbuttonClass = 'mgar';
+        this.RatePlanbuttonMessage= 'Possible Action Required';
+      }
+      if (this.deviceDetails?.Migratable_by_rate_plan == "Non-Migratable") {
+        this.RatePlanbuttonClass = 'nmg';
+        this.RatePlanbuttonMessage = 'Action Required';
+      }
+
+
+
+    });
   }
-
+  goBack() {
+    this.location.back();
+  }
+  ngOnInit(): void {}
 }
--- src/app/dashboard/device-rates/devices-rates-list/devices-rates-list.component.ts
+++ src/app/dashboard/device-rates/devices-rates-list/devices-rates-list.component.ts
@@ -2,100 +2,187 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UtilitiesService, deviceRatesState } from 'src/app/shared/services/utilities.service';
+import {
+  UtilitiesService,
+  deviceRatesState,
+} from 'src/app/shared/services/utilities.service';
 
-
-
-const ELEMENT_DATA: any[] = [
-  //   {"sno":1,"dvc_make":"Samsung","dvc_model":"SM-A125U",
-  //  "dvc_category":"SMART_PHONE - Basic","Authorized_Users":"GAL A12",
-  //  "Effective_Date":"2021-12-16","phone_number":9394600573,
-  //  "Billing_Account_Name":"LEONARDO CINTRON ASENCIO",
-  //  "Billing_Address_1":"SIERRA BAYAMON CALLE 21  BLQ18 20",
-  //  "Billing_Address_2":"","City":"BAYAMON","Service_ID":"Liberty Regular",
-  //  "Account_Type":"I","Account_Email":"LENNYCINTRON@ICLOUD.COM",
-  //  "Billing_Cycle":17,"Billing_Close_Date":17,"dp_prd_cd":"LPMHGH5",
-  //  "pp_prd_cd":"SDDVRP","Migratable_by_rate_plan":"Migratable",
-  //  "Migratable_By_Device":"Migratable","Action_Needed":"Complete Migratable"},
-  //  {"sno":16145,"dvc_make":"Samsung","dvc_model":"SM-A125U","dvc_category":"SMART_PHONE - Basic","Authorized_Users":"GAL A12","Effective_Date":"2021-12-16","phone_number":9394589265,"Billing_Account_Name":"LEONARDO CINTRON ASENCIO","Billing_Address_1":"SIERRA BAYAMON CALLE 21  BLQ18 20","Billing_Address_2":"","City":"BAYAMON","Service_ID":"Liberty Regular","Account_Type":"I","Account_Email":"LENNYCINTRON@ICLOUD.COM","Billing_Cycle":17,"Billing_Close_Date":17,"dp_prd_cd":"LPMHGH5","pp_prd_cd":"SDDVRP","Migratable_by_rate_plan":"Migratable","Migratable_By_Device":"Migratable","Action_Needed":"Complete Migratable"},{"sno":20969,"dvc_make":"Samsung","dvc_model":"SM-A125U","dvc_category":"SMART_PHONE - Basic","Authorized_Users":"GAL A12","Effective_Date":"2021-12-16","phone_number":9394992579,"Billing_Account_Name":"LEONARDO CINTRON ASENCIO","Billing_Address_1":"SIERRA BAYAMON CALLE 21  BLQ18 20","Billing_Address_2":"","City":"BAYAMON","Service_ID":"Liberty Regular","Account_Type":"I","Account_Email":"LENNYCINTRON@ICLOUD.COM","Billing_Cycle":17,"Billing_Close_Date":17,"dp_prd_cd":"LPMHGH5","pp_prd_cd":"SDDVRP","Migratable_by_rate_plan":"Migratable","Migratable_By_Device":"Migratable","Action_Needed":"Complete Migratable"}
-
-];
+const ELEMENT_DATA: any[] = [];
 
 @Component({
   selector: 'app-devices-rates-list',
   templateUrl: './devices-rates-list.component.html',
-  styleUrls: ['./devices-rates-list.component.scss']
+  styleUrls: ['./devices-rates-list.component.scss'],
 })
 export class DevicesRatesListComponent implements OnInit {
-
-  displayedColumns: string[] = ['Billing_Account_Name', 'phone_number', 'Effective_Date','Migratable_By_Device', 'device', 'button'];
+  displayedColumns: string[] = [
+    'Billing_Account_Name',
+    'phone_number',
+    'Effective_Date',
+    'Migratable_By_Device_status_code',
+    'device',
+    'button',
+  ];
   dataSource = new MatTableDataSource<deviceRatesState>(ELEMENT_DATA);
 
   isLoaded = false;
   length!: number;
+
+  DevicebuttonClass: any;
+  DevicebuttonMessage: any;
+
+  SimGenbuttonClass: any;
+  SimGenbuttonMessage: any;
+
+  RatePlanbuttonClass: any;
+  RatePlanbuttonMessage: any;
+
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
-migratableBy:Array<string>=[];
-  constructor(private util: UtilitiesService, private router: Router, private actRoute: ActivatedRoute) {
-    this.util.deviceRatesObservable$.subscribe(val => {
-      console.log('  deviceRatesObservable$ :', val);
-      
+  migratableBy: Array<string> = [];
+  constructor(
+    private util: UtilitiesService,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) {
+    this.util.deviceRatesObservable$.subscribe((val) => {
+      // console.log('  deviceRatesObservable$ :', val);
 
       if (val?.length > 0) {
-
         this.migratableBy = [];
-       
-          this.isLoaded = true;
+
+        this.isLoaded = true;
+
+        //Complete Migratable //Non-Migratable by SIM Status //Non Migratable by Rate Plan //Non Migratable by Device //NMG By Both Device and Rate Plan
+        val?.sort((a: any, b: any) =>
+          a.Migratable_By_Device_message > b.Migratable_By_Device_message
+            ? 1
+            : -1
+        );
+        this.dataSource = new MatTableDataSource<deviceRatesState>(val);
+        console.log('this val', val);
+
+        this.dataSource.paginator = this.paginator;
+        this.length = val.length;
+
+        val.map((inrVal: any) => {
+          if (
+            inrVal?.Migratable_By_Device_Config === 'Non-Migratable' ||
+            inrVal?.Migratable_by_Device_Config === null
+          ) {
+            this.migratableBy.push('Device_Config');
+          }
+
+          if (inrVal?.Migratable_by_rate_plan === 'Non-Migratable') {
+            this.migratableBy.push('Rate');
+          }
+          if (inrVal?.Migratable_By_Device === 'Non-Migratable') {
+            this.migratableBy.push('Device');
+          }
+
+          if (inrVal?.SIM_Gen_Status === 'Non-Migratable') {
+            this.migratableBy.push('Sim');
+          }
+
+          if (inrVal?.Migratable_by_rate_plan == 'Possible_Action_Required') {
+            this.migratableBy.push('Rate_mgar');
+          }
+          if (
+            inrVal?.Migratable_By_Device_message == 'Possible_Action_Required'
+          ) {
+            this.migratableBy.push('Device_mgar');
+          }
+
+          if (inrVal?.SIM_Gen_Status == 'Possible_Action_Required') {
+            this.migratableBy.push('Sim_mgar');
+          }
+        });
+        console.log('Inside array of this', this.migratableBy);
+        var uniqueMigratableBy = this.migratableBy.filter(
+          (v, i, a) => a.indexOf(v) === i
+        );
+        console.log('Inside array of filter', uniqueMigratableBy);
+
+      //  var res = uniqueMigratableBy.filter((item: string | string[]) => !item.includes("mgar"));
+//console.log("hui hui hui",res)
+
+
+        if (uniqueMigratableBy.includes('Device')) {
+          this.DevicebuttonClass='nmg';
+          this.DevicebuttonMessage='Action Required';
+          uniqueMigratableBy = uniqueMigratableBy.filter(
+            (e) => e !== 'Device_mgar'
+          );
+          console.log("line 116",uniqueMigratableBy)
+        
+        }
+        if (uniqueMigratableBy.includes('Device_mgar')) {
+          this.DevicebuttonClass='mgar';
+          this.DevicebuttonMessage='Possible Action Required';
+          console.log("line 122",uniqueMigratableBy)
        
         
-        //Complete Migratable //Non-Migratable by SIM Status //Non Migratable by Rate Plan //Non Migratable by Device //NMG By Both Device and Rate Plan
-        val?.sort((a:any, b:any) => (a.Migratable_By_Device > b.Migratable_By_Device? -1 : 1))
-        this.dataSource = new MatTableDataSource<deviceRatesState>(val);
-        console.log("this val",val)
-      
+        }
+        if (!uniqueMigratableBy.includes('Device_mgar') && !uniqueMigratableBy.includes('Device')) {
+          this.DevicebuttonClass='mg';
+          this.DevicebuttonMessage='Ready';
+          console.log("line 129",uniqueMigratableBy)
+        }
 
-       
-        this.dataSource.paginator = this.paginator;
-        this.length = val.length
-       
-        val.map((inrVal:any)=>{
-
-
-          if(inrVal?.Migratable_By_Device_Config
-            ==="Non-Migratable" || inrVal?.Migratable_by_Device_Config===null){
-            this.migratableBy.push('Device_Config')
-          }
-
-          if(inrVal?.Migratable_by_rate_plan
-            ==="Non-Migratable" || inrVal?.Migratable_by_rate_plan===null){
-            this.migratableBy.push('Rate')
-          }
-          if(inrVal?.Migratable_By_Device
-                ==="Non-Migratable" || inrVal?.Migratable_By_Device===null){
-            this.migratableBy.push('Device')
-          }
-         
-          if(inrVal?.SIM_Gen_Status==="Non-Migratable" || inrVal?.SIM_Gen_Status===null){
-            this.migratableBy.push('Sim')
-          }
+        if (uniqueMigratableBy.includes('Sim')) {
+          this.SimGenbuttonClass = 'nmg';
+          this.SimGenbuttonMessage = 'Action Required';
+          uniqueMigratableBy = uniqueMigratableBy.filter(
+            (e) => e !== 'Sim_mgar'
+          );
+        }
+        if (uniqueMigratableBy.includes('Sim_mgar')) {
+          this.SimGenbuttonClass = 'mgar';
+          this.SimGenbuttonMessage = 'Possible Action Required';
           
-        })
-      console.log("Inside array",this.migratableBy)
-      }else{
+        }
+        if (
+          !uniqueMigratableBy.includes('Sim_mgar') &&
+          !uniqueMigratableBy.includes('Sim')
+        ) {
+          this.SimGenbuttonClass = 'mg';
+          this.SimGenbuttonMessage = 'Ready';
+        }
+
+
+        if (uniqueMigratableBy.includes('Rate')) {
+          this.RatePlanbuttonClass='nmg'
+          this.RatePlanbuttonMessage='Action Required'
+          uniqueMigratableBy = uniqueMigratableBy.filter(
+            (e) => e !== 'Rate_mgar'
+          );
+        }
+        if (uniqueMigratableBy.includes('Rate_mgar')) {
+          this.RatePlanbuttonClass='mgar'
+          this.RatePlanbuttonMessage='Possible Action Required'
+          
+        }
+        if (!uniqueMigratableBy.includes('Rate_mgar') && !uniqueMigratableBy.includes('Rate')) {
+          this.RatePlanbuttonClass='mg'
+          this.RatePlanbuttonMessage='Ready'
+        
+        }
+
+        console.log('final ', uniqueMigratableBy);
+        console.log("device class",this.DevicebuttonClass)
+      } else {
         this.dataSource = new MatTableDataSource<deviceRatesState>([]);
         this.dataSource.paginator = this.paginator;
         this.length = 0;
       }
-    })
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   nextscreen(item: any) {
     this.router.navigate([`../details/${item.sno}`], {
-      relativeTo: this.actRoute
+      relativeTo: this.actRoute,
     });
   }
 }
-
