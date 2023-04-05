913f06dcb782b4b51c816abab620739474c8fb86
amanraj-29 <amanraj.a@prodapt.com>
2023-04-05 16:45:04 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   SESSION_EXPIRY_TIME:5,
- BASE_URL :`https://0g7q02pxm0.execute-api.us-east-1.amazonaws.com/dev/`,
- //BASE_URL: `https://cpfug7qjd1.execute-api.us-east-1.amazonaws.com/prod/`,
+ //BASE_URL :`https://0g7q02pxm0.execute-api.us-east-1.amazonaws.com/dev/`,
+ BASE_URL: `https://cpfug7qjd1.execute-api.us-east-1.amazonaws.com/prod/`,
   login:`signinad`
 };
